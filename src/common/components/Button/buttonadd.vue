c99d77caad730a02e2208aeb0ef6567c17e117bc
robku96 <robertkulicki1@wp.pl>
2017-07-29 14:50:17 +0200

    8 lines in file buttonadd for robert branch

--- src/common/components/Button/buttonadd.vue
+++ src/common/components/Button/buttonadd.vue
@@ -2,3 +2,7 @@
 <!-- some text for git test 2-->
 <!-- some text for git test 3-->
 <!-- some text for git test 4-->
+<!-- some text for git test 5-->
+<!-- some text for git test 6-->
+<!-- some text for git test 7-->
+<!-- some text for git test 8-->
