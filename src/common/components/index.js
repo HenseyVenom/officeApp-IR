6803b693041bbe7aadbd6e597b3421a27dce1820
i.baczewska <izabela.baczewska@gmail.com>
2017-07-31 17:02:06 +0200

    a

--- src/common/components/index.js
+++ src/common/components/index.js
@@ -6,23 +6,17 @@ import ButtonSwitchCompany from './Buttons/ButtonSwitchCompany.vue';
 import InputLoginPanel from './Inputs/InputLoginPanel.vue';
 import InputSearch from './Inputs/InputSearch.vue';
 import LoginHeader from './LoginHeader/LoginHeader.vue';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import NavigationBar from './NavigationBar/NavigationBar.vue';
 import ToolBar from './ToolBar/ToolBar.vue';
 import ToolBarItem from './ToolBar/ToolBarItem.vue';
 import UserAddForm from './UserAddForm/UserAddForm.vue';
 import InputSelect from './Inputs/InputSelect.vue';
 import InputCheckBox from './Inputs/InputCheckBox.vue';
-=======
-=======
->>>>>>> 3ec3b25ff619ba6aaa1c49e9ae206f661e35b2b2
 import NavigationBarTop from './NavigationBar/NavigationBarTop.vue';
 import NavigationBarBottom from './NavigationBar/NavigationBarBottom.vue';
 import NavigationItem from './NavigationBar/NavigationItem.vue';
 
 //import UserAddForm from '.UserAddForm/UserAddForm.vue';
->>>>>>> 3ec3b25ff619ba6aaa1c49e9ae206f661e35b2b2
 
 Vue.component(ButtonSignIn.name, ButtonSignIn);
 Vue.component(InputLoginPanel.name, InputLoginPanel);
@@ -30,19 +24,13 @@ Vue.component(InputSearch.name, InputSearch);
 Vue.component(LoginHeader.name, LoginHeader);
 Vue.component(ButtonLogOut.name, ButtonLogOut);
 Vue.component(ButtonSwitchCompany.name, ButtonSwitchCompany);
-<<<<<<< HEAD
-<<<<<<< HEAD
 Vue.component(NavigationBar.name, NavigationBar);
 Vue.component(ToolBar.name, ToolBar);
 Vue.component(ToolBarItem.name, ToolBarItem);
 Vue.component(UserAddForm.name, UserAddForm);
 Vue.component(InputSelect.name, InputSelect);
 Vue.component(InputCheckBox.name, InputCheckBox);
-=======
-=======
->>>>>>> 3ec3b25ff619ba6aaa1c49e9ae206f661e35b2b2
 Vue.component(NavigationBarTop.name, NavigationBarTop);
 Vue.component(NavigationBarBottom.name, NavigationBarBottom);
 Vue.component(NavigationItem.name, NavigationItem);
 //Vue.component(UserAddForm.name, UserAddForm);
->>>>>>> 3ec3b25ff619ba6aaa1c49e9ae206f661e35b2b2
